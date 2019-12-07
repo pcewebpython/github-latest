f7774306dae88c50baa1afb5628c2b83b320e73b
Paul Jurek <pjurek3@gmail.com>
2019-12-06 20:28:15 -0800

    completed exercise

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import json
 import requests
 
 # Use Like python githubber.py JASchilz
+def get_users_events(username):
+    """returns a list of events for user from github"""
+    url = f"https://api.github.com/users/{username}/events"
+    response = requests.get(url)
+    return response.json()
 # (or another user name)
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
+    events = get_users_events(username)
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    print(events[0]['created_at'])
 
-    print("COMPLETE THE TODOs")
     
 
 
