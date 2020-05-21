c085b6db310c441a0be5c50fc751804eb99d0b2a
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-05-21 15:56:54 -0700

    Finished github-latest

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
+# main.py
+
 import sys
 import json
-
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
+# Use like python main.py username
+# Ex. python main.py ramr
 
 if __name__ == "__main__":
     username = sys.argv[1]
-
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
+    # Retrieve a list of "events" associated with the given user name
     response = requests.get("https://api.github.com/users/{}/events".format(username))
-    # 2. Print out the time stamp associated with the first event in that list.
     events = json.loads(response.content)
 
+    # Print out the time stamp associated with the first event in that list
     print(events[0]['created_at'])
