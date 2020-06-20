d3d7199f4b51ae38473ce0f91495cf91834fe412
Zhen Yang <wwxyzd@hotmail.com>
2020-06-20 12:13:02 -0700

    finished activity for lesson06

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+#1. Retrieve a list of "events" associated with the given user name
+events = requests.get('https://api.github.com/users/{}/events'.format(username))
 
-    print("COMPLETE THE TODOs")
-    
-
-
+# 2. if the status_code is 200 and the returned events list is not empty, then
+# print out the time stamp associated with the first event in that list
+if events.status_code:
+    if events.json():
+        print(f"User:{username} create first event at: {events.json()[0]['created_at']}")
+    else:
+        print(f"The events list for user:\"{username}\" is empty.")
+else:
+    print(f"Couldn't find the user:\"{username}\"")
