5dff0b75834ec74497778f3b0801126c30969ffa
stellie <kims82@gmail.com>
2020-12-18 14:38:11 -0800

    complete activity to retrieve user events from Github

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
+# Stella Kim
+# Activity 6: Github API
+
 import sys
 import json
-
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
 
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
-    
-
-
+    response = requests.get(
+        'https://api.github.com/users/{}/events'.format(username))
+    events = json.loads(response.content)
+    print(events)  # events associated with given username
+    print(events[0]['created_at'])  # first event timestamp in list
