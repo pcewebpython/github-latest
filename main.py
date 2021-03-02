4a97ac914c271dcc34e258adef33e5116237f042
Jason Jenkins <jdj0891@yahoo.com>
2021-03-02 09:37:31 -0800

    Complete

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import requests
 
 # Use Like python githubber.py JASchilz
 # (or another user name)
+# https://api.github.com/
 
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
     # 1. Retrieve a list of "events" associated with the given user name
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+
     # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
-    
-
-
+    events = json.loads(response.content)
+    print(events[0]['created_at'])
