7ca70dff84965cf7647025336ffea1cdc11d86cb
Sean McKellips <mckellip@adobe.com>
2021-04-11 22:39:28 -0700

    add activity

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ if __name__ == "__main__":
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
 
-    print("COMPLETE THE TODOs")
+    # print("COMPLETE THE TODOs")
     
+    api_url = 'https://api.github.com'
+    try:
+        events = requests.get(f'{api_url}/users/{username}/events')
+        first = events.json()[0]['created_at']
+    except:
+        first = None
 
-
+    if first:
+        print(f"First event for user {username} was at {first}")
+    else:
+        print("Unable to retrieve timestamp for user.")
